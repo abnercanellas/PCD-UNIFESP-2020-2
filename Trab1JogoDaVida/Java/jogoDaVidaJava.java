a93ba59df31d3e6b7b4cf777fa52926860bbbd1a
Abner Canellas <abnercanellas@gmail.com>
2020-12-03 14:23:18 -0300

    trabJavaInit

--- Trab1JogoDaVida/Java/jogoDaVidaJava.java
+++ Trab1JogoDaVida/Java/jogoDaVidaJava.java
@@ -1,18 +1,17 @@
 package Trab1JogoDaVida.Java;
 
 import java.util.Random;
-import java.util.Vector;
 
 public class jogoDaVidaJava {
 
     private int DIMENTION = 10; //2048*2048​ e um total de ​2000​ geracoes
-    private int NGENERATIONS = 2048;
+    //private int NGENERATIONS = 2048;
 
-    public void populate(Vector<Integer[][]> grid){
+    public void populate(Integer[][] grid){
         Random gerador = new Random(1985);
         for(int i = 0; i<DIMENTION; i++) { //laço sobre as células do tabuleiro sem contar comum eventual halo
-                for(j = 0; j<DIMENTION; j++)
-                    {grid[i][j] = gerador.nextInt(2147483647) % 2;
+                for(int j = 0; j<DIMENTION; j++){
+                    grid[i][j] = gerador.nextInt(2147483647) % 2;
             }
         }
     }
